7a8890736d004c5a82c0dce0def1ed766fe9efe7
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-04-25 01:37:40 -0700

    set up confugration

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,2 @@
-file{'/tmp/school': ensure =>file,content => 'I love Puppet',group => 'www-data',owner => 'www-data',mode => '0744'}
+# This manifest creates a file school at /tmp
+file {'/tmp/school': ensure =>file,content => 'I love Puppet',group => 'www-data',owner => 'www-data',mode => '0744',}
--- 0x0A-configuration_management/README.md
+++ 0x0A-configuration_management/README.md
@@ -1,1 +1,1 @@
-
+This project is all about server confugration using confugration tool CM puppet
