c5b5beeb82aa19096b3d87e1841efbbdd331204c
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-06-07 04:53:02 +0000

    advance api cals

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    user_agent = {'User-agent': 'Google Chrome Version 114.0.5735.90'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     res = get(url, headers=user_agent)
     results = res.json()
