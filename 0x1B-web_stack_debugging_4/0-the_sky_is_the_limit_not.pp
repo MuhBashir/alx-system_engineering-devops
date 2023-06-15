b177a1b315224c507ce5fc271ee89f12adc0c98d
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-06-15 22:00:49 +0000

    debugging 4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+This script increases the traffic of an Nginx server 
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx server
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+# this project is all about improving an nginx server using puppet
