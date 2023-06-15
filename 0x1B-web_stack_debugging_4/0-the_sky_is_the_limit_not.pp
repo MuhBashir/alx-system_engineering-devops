a7c86044c3a1b858732c925c860b821c4d460472
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2023-06-15 22:12:41 +0000

    debugging 4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,7 @@
-This script increases the traffic of an Nginx server 
+# This script increases the traffic of an Nginx server 
 
+
+# Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
